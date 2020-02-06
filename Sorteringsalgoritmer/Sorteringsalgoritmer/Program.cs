e8670ce0c85c6f455e1a96a8b4fd7e6633859971
Vendela Herz <vehe01001@utb.vaxjo.se>
2020-02-06 12:47:09 +0100

    Bubblesort

---
+++ Sorteringsalgoritmer/Sorteringsalgoritmer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace Sorteringsalgoritmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sortering sortering = new Sortering();
+            List<int> nummerlista = new List<int>();
+            Random nummer = new Random();
+            for (int i = 0; i < 1_0; i++)
+            {
+                int num = nummer.Next(1, 100001);
+                nummerlista.Add(num);
+            }
+            List<int> bubblesort = new List<int>(nummerlista);
+
+            double bubbletime = sortering.Bubble(bubblesort);
+            Console.WriteLine("Bubblesort tid: " + bubbletime + " millisekunder");
+
+        }
+        
+    }
+}
+    
---
+++ Sorteringsalgoritmer/Sorteringsalgoritmer/Sortering.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace Sorteringsalgoritmer
+{
+    class Sortering
+    {
+        public double Bubble(List<int> bubblesort)
+        {
+            Stopwatch bubblewatch = new Stopwatch();
+            bubblewatch.Start();
+            for (int i = 0; i < bubblesort.Count - 1; i++)
+            {
+                for (int j = 0; j < bubblesort.Count - 1; j++)
+                {
+                    if (bubblesort[j] > bubblesort[j + 1])
+                    {
+                        int temp = bubblesort[j];
+                        bubblesort[j] = bubblesort[j + 1];
+                        bubblesort[j + 1] = temp;
+                    }
+                }
+            }
+            bubblewatch.Stop();
+            return bubblewatch.Elapsed.TotalMilliseconds;
+        }
+    }
+}
